0914b6b7c0b775c9a7431f5165d50667b848eac6
Pedro Ortiz <pedrops.ortiz@gmail.com>
2022-10-29 09:07:22 -0300

    Criação da Rest API para a biblioteca em Java através do Spring Initializer

---
+++ src/main/java/br/ufsm/poow2/biblioteca_rest/BibliotecaRestApplication.java
@@ -0,0 +1,13 @@
+package br.ufsm.poow2.biblioteca_rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibliotecaRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BibliotecaRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/ufsm/poow2/biblioteca_rest/BibliotecaRestApplicationTests.java
@@ -0,0 +1,13 @@
+package br.ufsm.poow2.biblioteca_rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibliotecaRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
