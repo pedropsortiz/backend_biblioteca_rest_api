9a9afb890fb6efb41b77a085f9cc9a4e94a5dc62
Pedro Ortiz <pedrops.ortiz@gmail.com>
2022-11-01 22:04:03 -0300

    Criação dos models do banco de dados

---
+++ src/main/java/br/ufsm/poow2/biblioteca_rest/Model/Autor.java
@@ -0,0 +1,73 @@
+package br.ufsm.poow2.biblioteca_rest.Model;
+
+import java.sql.Date;
+
+public class Autor {
+
+    private int idAutor;
+    private String nomeAutor;
+    private Date dataNascAutor;
+    private Date dataMorteAutor;
+    private String descricaoAutor;
+    private String fotoAutor;
+
+    public Autor() {
+    }
+
+    public Autor(int idAutor, String nomeAutor, Date dataNascAutor, Date dataMorteAutor, String descricaoAutor, String fotoAutor) {
+        this.idAutor = idAutor;
+        this.nomeAutor = nomeAutor;
+        this.dataNascAutor = dataNascAutor;
+        this.dataMorteAutor = dataMorteAutor;
+        this.descricaoAutor = descricaoAutor;
+        this.fotoAutor = fotoAutor;
+    }
+
+    public int getIdAutor() {
+        return idAutor;
+    }
+
+    public void setIdAutor(int idAutor) {
+        this.idAutor = idAutor;
+    }
+
+    public String getNomeAutor() {
+        return nomeAutor;
+    }
+
+    public void setNomeAutor(String nomeAutor) {
+        this.nomeAutor = nomeAutor;
+    }
+
+    public Date getDataNascAutor() {
+        return dataNascAutor;
+    }
+
+    public void setDataNascAutor(Date dataNascAutor) {
+        this.dataNascAutor = dataNascAutor;
+    }
+
+    public Date getDataMorteAutor() {
+        return dataMorteAutor;
+    }
+
+    public void setDataMorteAutor(Date dataMorteAutor) {
+        this.dataMorteAutor = dataMorteAutor;
+    }
+
+    public String getDescricaoAutor() {
+        return descricaoAutor;
+    }
+
+    public void setDescricaoAutor(String descricaoAutor) {
+        this.descricaoAutor = descricaoAutor;
+    }
+
+    public String getFotoAutor() {
+        return fotoAutor;
+    }
+
+    public void setFotoAutor(String fotoAutor) {
+        this.fotoAutor = fotoAutor;
+    }
+}
---
+++ src/main/java/br/ufsm/poow2/biblioteca_rest/Model/Genero.java
@@ -0,0 +1,31 @@
+package br.ufsm.poow2.biblioteca_rest.Model;
+
+public class Genero {
+
+    private int idGenero;
+    private String nomeGenero;
+
+    public Genero() {
+    }
+
+    public Genero(int idGenero, String nomeGenero) {
+        this.idGenero = idGenero;
+        this.nomeGenero = nomeGenero;
+    }
+
+    public int getIdGenero() {
+        return idGenero;
+    }
+
+    public void setIdGenero(int idGenero) {
+        this.idGenero = idGenero;
+    }
+
+    public String getNomeGenero() {
+        return nomeGenero;
+    }
+
+    public void setNomeGenero(String nomeGenero) {
+        this.nomeGenero = nomeGenero;
+    }
+}
