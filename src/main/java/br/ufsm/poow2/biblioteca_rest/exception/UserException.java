c3112edd980de9c42d757d3a77c0e05d067d04ec
Pedro Ortiz <pedrops.ortiz@gmail.com>
2022-12-30 12:23:35 -0300

    Atualização do User e criação do package Exception para tratar erros e limpar o espaço do Service

---
+++ src/main/java/br/ufsm/poow2/biblioteca_rest/exception/UserException.java
@@ -0,0 +1,121 @@
+package br.ufsm.poow2.biblioteca_rest.exception;
+
+import br.ufsm.poow2.biblioteca_rest.model.User;
+import br.ufsm.poow2.biblioteca_rest.repository.UserRepository;
+import org.apache.commons.validator.routines.EmailValidator;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@ControllerAdvice
+public class UserException {
+
+    @Autowired
+    UserRepository userRepository;
+    private static final String NAME_REGEX = "^[A-Za-z\\u00C0-\\u017FÇç]+$";
+    private static final String PASSWORD_REGEX = "^(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*()_+-=])[A-Za-z0-9!@#$%^&*()_+-=]{8,}$";
+
+    @ExceptionHandler(Exception.class)
+    public List<String> handleAddUserErrors(User user) {
+        List<String> errors = new ArrayList<>();
+
+        if (!isEmailValid(user.getEmail())) {
+            errors.add("Email inválido. O email deve conter um nome de usuário seguido de um sinal '@' e um domínio, por exemplo, 'usuario@dominio.com'");
+        }
+
+        if (doesUserExistByEmail(user.getEmail())) {
+            errors.add("Email já em uso");
+        }
+
+        if (!isPermissionValid(user.getPermission())) {
+            errors.add("Papéis de usuário não reconhecidos.");
+        }
+
+        if (!isNameValid((user.getName()))) {
+            errors.add("Nome de usuário inválido. O nome de usuário pode conter apenas letras acentos e o caractere 'ç'");
+        }
+
+        if (!isPasswordValid(user.getPassword())) {
+            errors.add("Senha inválida. A senha deve ter pelo menos 8 caracteres e deve conter pelo menos uma letra maiúscula, um número e um caractere especial");
+        }
+
+        return errors;
+    }
+
+    @ExceptionHandler(Exception.class)
+    public List<String> handleUpdateUserErrors(Integer id, User user) {
+        List<String> errors = new ArrayList<>();
+        User userRepo = userRepository.findById(id).orElse(null);
+
+        if (userRepo.equals(null)){
+            errors.add("Usuário não encontrado ou não existe!");
+        }else{
+            if (user.getPassword().equals(userRepo.getPassword())){
+                errors = handleAddUserErrors(user);
+            }else {
+                errors.add("Erro ao atualizar perfil. Senha incorreta");
+            }
+        }
+
+        return errors;
+    }
+
+    @ExceptionHandler(Exception.class)
+    public List<String> handleDeleteUserErrors(Integer id) {
+        List<String> errors = new ArrayList<>();
+
+        if (id == null){
+            errors.add("O ID do usuário não pode ser nulo.");
+        }
+        else if (!doesUserExistById(id)){
+            errors.add("O usuário especificado não foi encontrado ou não existe.");
+        }
+
+        return errors;
+    }
+
+    @ExceptionHandler(Exception.class)
+    public List<String> handleUpdateTokenErros(String email, String jwtToken) {
+        List<String> errors = new ArrayList<>();
+
+        if (email == null || email.trim().isEmpty()) {
+            errors.add("O email não pode ser nulo ou vazio");
+        }
+        if (jwtToken == null || jwtToken.trim().isEmpty()) {
+            errors.add("O token JWT não pode ser nulo ou vazio");
+        }
+        if (!doesUserExistByEmail(email)) {
+            errors.add("O usuário com o email especificado não foi encontrado");
+        }
+
+        return errors;
+    }
+
+    public boolean doesUserExistById(Integer id) {
+        return userRepository.findById(id) != null;
+    }
+
+    public boolean doesUserExistByEmail(String email) {
+        return userRepository.findUserByEmail(email) != null;
+    }
+
+    public boolean isEmailValid(String email) {
+        return EmailValidator.getInstance().isValid(email);
+    }
+
+    public boolean isPermissionValid(String permission) {
+        return permission == "USR"|| permission == "ADM";
+    }
+
+    public boolean isNameValid(String name) {
+        return name != null && name.matches(NAME_REGEX);
+    }
+
+    public boolean isPasswordValid(String password) {
+        return password != null && password.matches(PASSWORD_REGEX);
+    }
+
+}
--- src/main/java/br/ufsm/poow2/biblioteca_rest/service/UserService.java
+++ src/main/java/br/ufsm/poow2/biblioteca_rest/service/UserService.java
@@ -1,8 +1,10 @@
 package br.ufsm.poow2.biblioteca_rest.service;
 
 import br.ufsm.poow2.biblioteca_rest.common.ApiResponse;
+import br.ufsm.poow2.biblioteca_rest.exception.UserException;
 import br.ufsm.poow2.biblioteca_rest.model.User;
 import br.ufsm.poow2.biblioteca_rest.repository.UserRepository;
+import br.ufsm.poow2.biblioteca_rest.security.JWTUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
 import org.springframework.http.HttpStatus;
@@ -20,90 +22,44 @@ public class UserService {
     @Autowired
     UserRepository userRepository;
 
+    @Autowired
+    UserException userException;
+
     public ResponseEntity<ApiResponse> addUser(User user) {
-        String password = user.getPassword();
-        String email = user.getEmail();
-        String permission = user.getPermission();
+        ResponseEntity<ApiResponse> response;
+        List<String> handleErrors = userException.handleAddUserErrors(user);
 
-        // Validate email
-        final String emailRegex = "^[\\w-_\\.+]*[\\w-_\\.]@([\\w]+\\.)+[\\w]+[\\w]$";
-        if (!email.matches(emailRegex)) {
-            return new ResponseEntity<>(
-                    new ApiResponse(false, "Failed to create new user. Invalid email"),
-                    HttpStatus.BAD_REQUEST
-            );
-        }
-
-        // Check if email is already in use
-        User existingEmail = userRepository.findUserByEmail(email);
-        if (existingEmail != null) {
-            return new ResponseEntity<>(
-                    new ApiResponse(false, "Failed to create new user. Email already in use"),
-                    HttpStatus.BAD_REQUEST
-            );
-        }
-
-        // Validate user role
-        if (!permission.equals("usr") && !permission.equals("adm")) {
-            return new ResponseEntity<>(
-                    new ApiResponse(false, "Failed to create new user. Unrecognized user roles."),
-                    HttpStatus.BAD_REQUEST
-            );
-        }
-
-        // Validate user name
-        final String nameRegex = "^[A-Za-z\\u00C0-\\u017F]+$";
-        if (!user.getName().matches(nameRegex)) {
-            return new ResponseEntity<>(
-                    new ApiResponse(false, "Failed to create new user. Invalid user name"),
-                    HttpStatus.BAD_REQUEST
-            );
-        }
-
-        // Validate password
-        final String passwordRegex = "^(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*()_+-=])[A-Za-z0-9!@#$%^&*()_+-=]{8,}$";
-        if (!password.matches(passwordRegex)) {
-            return new ResponseEntity<>(
-                    new ApiResponse(false, "Failed to create new user. Invalid password"),
-                    HttpStatus.BAD_REQUEST
-            );
-        }
-
-        // Attempt to create a new user
-        try {
-            // Encode user password using BCrypt
-            user.setPassword(new BCryptPasswordEncoder().encode(password));
-            user.setToken(null);
-
-            // Save user to repository
-            userRepository.save(user);
-            return new ResponseEntity<>(
-                    new ApiResponse(true, "New user created successfully!"), HttpStatus.CREATED);
-        } catch (Exception e) {
-            HttpStatus status;
-            String message;
-
-            // Determine error type based on thrown exception
-            if (e instanceof DataAccessException) {
-                status = HttpStatus.INTERNAL_SERVER_ERROR;
-                message = "Failed ao criar usuário devido a um problema de acesso aos dados: " + e.getMessage();
-            } else if (e instanceof ConstraintViolationException) {
-                status = HttpStatus.INTERNAL_SERVER_ERROR;
-                message = "Falha ao criar usuário devido a um problema de inicialização: " + e.getMessage();
-            } else {
-                // Trata qualquer outro tipo de exceção de forma genérica
-                status = HttpStatus.INTERNAL_SERVER_ERROR;
-                message = "Falha ao criar usuário devido a um erro desconhecido: " + e.getMessage();
+        if (handleErrors.isEmpty()){
+            try {
+                user.setPassword(new BCryptPasswordEncoder().encode(user.getPassword()));
+                user.setToken(new JWTUtil().geraToken(user));
+                userRepository.save(user);
+                response = ResponseEntity.status(HttpStatus.CREATED).body(
+                        new ApiResponse(true, "Novo usuário criado com sucesso!"));
+            } catch (DataAccessException e) {
+                response = ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(
+                        new ApiResponse(false, "Ocorreu um erro ao acessar o banco de dados. Por favor, tente novamente mais tarde."));
             }
-            // Retorna a resposta com os detalhes do erro
-            return new ResponseEntity<>(new ApiResponse(false, message), status);
         }
+        else
+        {
+            response = ResponseEntity.status(HttpStatus.BAD_REQUEST).body(
+                    new ApiResponse(false, "Falha ao criar novo usuário. " + String.join(" ", handleErrors))
+            );
+        }
+        return response;
     }
 
     public void updateJwtToken(String email, String jwtToken) {
-        User user = userRepository.findUserByEmail(email);
-        user.setToken(jwtToken);
-        userRepository.save(user);
+        List<String> handleErrors = userException.handleUpdateTokenErros(email, jwtToken);
+        if (handleErrors.isEmpty()) {
+            User user = userRepository.findUserByEmail(email);
+            user.setToken(jwtToken);
+            userRepository.save(user);
+        }
+        else{
+            System.out.println(handleErrors);
+        }
     }
 
     public List<User> findAllUsers() {
@@ -112,82 +68,28 @@ public class UserService {
 
     public ResponseEntity<ApiResponse> updateUser(Integer id, User updatedUser) {
         Optional<User> user = userRepository.findById(id);
+        ResponseEntity<ApiResponse> response;
+        List<String> handleErrors = userException.handleUpdateUserErrors(id, updatedUser);
 
-        String password = updatedUser.getPassword();
-        String email = updatedUser.getEmail();
-        String permission = updatedUser.getPermission();
+        if (handleErrors.isEmpty()){
+            try {
+                user.get().update(updatedUser);
+                userRepository.save(user.get());
+                response = ResponseEntity.status(HttpStatus.CREATED).body(
+                        new ApiResponse(true, "Usuário editado com sucesso!"));
 
-        // Validate email
-        final String emailRegex = "^[\\w-_\\.+]*[\\w-_\\.]@([\\w]+\\.)+[\\w]+[\\w]$";
-        if (!email.matches(emailRegex)) {
-            return new ResponseEntity<>(
-                    new ApiResponse(false, "Failed to update user. Invalid email"),
-                    HttpStatus.BAD_REQUEST
-            );
-        }
-
-        // Check if email is already in use
-        User existingEmail = userRepository.findUserByEmail(email);
-        if (existingEmail != null) {
-            return new ResponseEntity<>(
-                    new ApiResponse(false, "Failed to update user. Email already in use"),
-                    HttpStatus.BAD_REQUEST
-            );
-        }
-
-        // Validate user role
-        if (!permission.equals("usr") && !permission.equals("adm")) {
-            return new ResponseEntity<>(
-                    new ApiResponse(false, "Falha ao editar usuário. Permissões de usuário não reconhecidas."),
-                    HttpStatus.BAD_REQUEST
-            );
-        }
-
-        // Validação de nome de usuário
-        final String nomeRegex = "^[A-Za-z\\u00C0-\\u017F]+$";
-        if (!updatedUser.getName().matches(nomeRegex)) {
-            return new ResponseEntity<>(
-                    new ApiResponse(false, "Falha ao editar usuário. Nome de usuário inválido"),
-                    HttpStatus.BAD_REQUEST
-            );
-        }
-
-        // Validação de senha
-        final String senhaRegex = "^(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*()_+-=])[A-Za-z0-9!@#$%^&*()_+-=]{8,}$";
-        if (!password.matches(senhaRegex)) {
-            return new ResponseEntity<>(
-                    new ApiResponse(false, "Falha ao editar usuário. Senha inválida"),
-                    HttpStatus.BAD_REQUEST
-            );
-        }
-
-        // Tentativa para criar um novo usuário
-        try {
-            // Codifica a senha do usuário usando BCrypt
-            user.get().update(updatedUser);
-            userRepository.save(user.get());
-
-            return new ResponseEntity<>(
-                    new ApiResponse(true, "Usuário editado com sucesso!"), HttpStatus.CREATED);
-        } catch (Exception e) {
-            HttpStatus status;
-            String mensagem;
-
-            // Determina o tipo de erro de acordo com a exceção lançada
-            if (e instanceof DataAccessException) {
-                status = HttpStatus.INTERNAL_SERVER_ERROR;
-                mensagem = "Falha ao editar usuário devido a um problema de acesso aos dados: " + e.getMessage();
-            } else if (e instanceof ConstraintViolationException) {
-                status = HttpStatus.INTERNAL_SERVER_ERROR;
-                mensagem = "Falha ao editar usuário devido a um problema de inicialização: " + e.getMessage();
-            } else {
-                // Trata qualquer outro tipo de exceção de forma genérica
-                status = HttpStatus.INTERNAL_SERVER_ERROR;
-                mensagem = "Falha ao editar usuário devido a um erro desconhecido: " + e.getMessage();
+            } catch (DataAccessException e) {
+                response = ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(
+                        new ApiResponse(false, "Ocorreu um erro ao acessar o banco de dados. Por favor, tente novamente mais tarde."));
             }
-            // Retorna a resposta com os detalhes do erro
-            return new ResponseEntity<>(new ApiResponse(false, mensagem), status);
         }
+        else
+        {
+            response = ResponseEntity.status(HttpStatus.BAD_REQUEST).body(
+                    new ApiResponse(false, "Falha ao atualizar usuário. " + String.join(" ", handleErrors))
+            );
+        }
+        return response;
 
     }
 
@@ -195,27 +97,28 @@ public class UserService {
         return userRepository.findUserByEmail(email);
     }
 
-    public boolean doesUserExists(Integer idUsuario) {
-        return userRepository.existsById(idUsuario);
-    }
-
     public ResponseEntity<ApiResponse> deleteUserById(Integer userId) {
-        // Verifica se o ID do usuário é nulo
-        if (userId == null) {
-            return new ResponseEntity<>(new ApiResponse(false, "User ID cannot be null."), HttpStatus.OK);
+        ResponseEntity<ApiResponse> response;
+        List<String> handleErrors = userException.handleDeleteUserErrors(userId);
+
+        if (handleErrors.isEmpty()){
+            try {
+                userRepository.deleteById(userId);
+                response = ResponseEntity.status(HttpStatus.CREATED).body(
+                        new ApiResponse(true, "Usuário deletado com sucesso!"));
+            } catch (DataAccessException e) {
+                response = ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(
+                        new ApiResponse(false, "Ocorreu um erro ao acessar o banco de dados. Por favor, tente novamente mais tarde."));
+            }
         }
-
-        // Verifica se o usuário com o ID especificado existe
-        Optional<User> userOptional = userRepository.findById(userId);
-        if (!userOptional.isPresent()) {
-            return new ResponseEntity<>(new ApiResponse(false, "User with specified ID does not exist."), HttpStatus.OK);
+        else
+        {
+            response = ResponseEntity.status(HttpStatus.BAD_REQUEST).body(
+                    new ApiResponse(false, "Falha ao deletar usuário. " + String.join(" ", handleErrors))
+            );
         }
+        return response;
 
-        // Exclui o usuário
-        userRepository.deleteById(userId);
-
-        // Retorna uma resposta da API com o resultado da operação
-        return new ResponseEntity<>(new ApiResponse(true, "User was successfully deleted!"), HttpStatus.OK);
     }
 
 }
